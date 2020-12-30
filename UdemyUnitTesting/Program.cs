974480f16c1a53432fc07d16361238033f092ef0
bmckw <bmckw@DESKTOP-S1NDEEH>
2020-12-29 19:17:41 -0500

    Add project files.

---
+++ UdemyUnitTesting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UdemyUnitTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
